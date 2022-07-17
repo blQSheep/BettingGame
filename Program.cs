c2617f35bfb6de3d758f14be78774db952517fa9
blQSheep <unit88dev@gmail.com>
2022-07-17 16:34:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace BettingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args, string v)
+        {
+            double odds = .75;
+            Random random = new Random();
+
+            Guy player = new Guy() { Name = "The player", Cash = 100 };
+            Console.WriteLine($"Welcome to the casino. The odds are {odds}");
+
+            while (player.Cash > 0)
+            {
+                player.WriteMyInfo();
+                Console.Write("How much do you want to bet: ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = player.GiveCash(amount) * 2;
+
+                    if (pot > 0)
+                    {
+                        if (random.NextDouble() < odds)
+                        {
+                            int winnings = pot;
+                            Console.WriteLine("You win " + winnings);
+                            player.ReceiveCash(winnings);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bad luck, you lose.");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{howMuch} is not a number. Please enter a valid number.");
+                }
+            }
+            Console.WriteLine("The house always wins.");
+
+        }
+    }
+}
